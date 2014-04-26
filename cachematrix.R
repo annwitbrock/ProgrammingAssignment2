524bc0260e47c6c1fd5dcee1443b28013c6178d6
Ann Witbrock <annwitbrock@googlemail.com>
2014-04-26 10:48:58 +0100

    functions makeCacheMatrix and cacheSolve for cacheing matrix inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair of functions makeCacheMatrix() and cacheSolve()
+## allow an invertible matrix and its inverse to be cached.
+## The inversion function used is solve() 
 
-## Write a short comment describing this function
+## makeCacheMatrix - returns a cacheable invertible matrix.
+##        x: is an invertible matrix
+##        set() stores the matrix
+##        get() retrieves the matrix
+##        setinverse caches the result of of the inversion function
+##        getinverse retrieves any cached result
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        
+        # store the matrix
+        set <- function(mx){
+                x <<- mx
+                inv <<- NULL
+        }
+        get <- function() x
+        
+        # cache the inverse matrix
+        setinverse <- function(s) inv <<- s
+        getinverse <- function() inv
+        
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve - returns the cached inverse of a matrix.
+##        x:  is a cacheable invertible matrix defined by calling
+##            makeCacheMatrix(M), where M is an invertible matrix
+##
+         
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        # get cached inverse if it exists
+        inv <- x$getinverse()            
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        
+        # create a cached inverse
+        data <- x$get()          # obtain the matrix
+        inv <- solve(data, ...)  # invert the matrix
+        x$setinverse(inv)        # cache the inverse
+        inv
 }
